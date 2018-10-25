d60e624fdc5488b35531556349f6ec9c3f4df3ad
tdeloris <tdeloris@companeo.com>
2018-10-25 15:55:19 +0200

    Updates

---
+++ precache-manifest.e96cbc76adbb9ab6da3bd090208258c1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e27794cc3b3adb37e043ef8bb56c7a3",
+    "url": "/nycity/static/media/imgEight.0e27794c.jpg"
+  },
+  {
+    "revision": "d3c299675e8fa722b9a0",
+    "url": "/nycity/static/css/main.3be8fd9d.chunk.css"
+  },
+  {
+    "revision": "eaba8a439764a39a17399643251e2b08",
+    "url": "/nycity/static/media/new-york-manhattan.eaba8a43.jpg"
+  },
+  {
+    "revision": "50ce103911f2b560e513",
+    "url": "/nycity/static/js/1.50ce1039.chunk.js"
+  },
+  {
+    "revision": "7e5637b1f485c6e7a1f3",
+    "url": "/nycity/static/js/runtime~main.7e5637b1.js"
+  },
+  {
+    "revision": "909a66eff99329376fa96f52f858ba1a",
+    "url": "/nycity/static/media/imgOne.909a66ef.jpg"
+  },
+  {
+    "revision": "31ff32f8fb35f4042bdc8dd460e23b9e",
+    "url": "/nycity/static/media/imgTwo.31ff32f8.jpg"
+  },
+  {
+    "revision": "d1ba0fb3ddd6bf0bf64b32e2508cc2ac",
+    "url": "/nycity/static/media/imgThree.d1ba0fb3.jpg"
+  },
+  {
+    "revision": "441d080fdfd673db44614a335a80c224",
+    "url": "/nycity/static/media/imgFour.441d080f.jpg"
+  },
+  {
+    "revision": "d8128a4fc1ccae892166dcbf1d86a6ac",
+    "url": "/nycity/static/media/imgFive.d8128a4f.jpg"
+  },
+  {
+    "revision": "dc600b71227d022c751e20b607631f87",
+    "url": "/nycity/static/media/imgSix.dc600b71.jpg"
+  },
+  {
+    "revision": "8d989eb377e3d3ad14b0cca2bcbede76",
+    "url": "/nycity/static/media/imgSeven.8d989eb3.jpg"
+  },
+  {
+    "revision": "d3c299675e8fa722b9a0",
+    "url": "/nycity/static/js/main.d3c29967.chunk.js"
+  },
+  {
+    "revision": "2928256571389f2d027f80edc919e0d5",
+    "url": "/nycity/static/media/imgNine.29282565.jpg"
+  },
+  {
+    "revision": "6fa2007fb3737860ba7e3e72a9d77dcc",
+    "url": "/nycity/static/media/imgOne.6fa2007f.jpg"
+  },
+  {
+    "revision": "4e2ab3fa6a3b243f404d5f46507050f9",
+    "url": "/nycity/static/media/imgTwo.4e2ab3fa.jpg"
+  },
+  {
+    "revision": "9778e454f12435b617ffd8d4814e6b0c",
+    "url": "/nycity/static/media/imgThree.9778e454.jpg"
+  },
+  {
+    "revision": "0146a4b31f3cda222ec1ac313acc9b92",
+    "url": "/nycity/static/media/imgFour.0146a4b3.jpg"
+  },
+  {
+    "revision": "062efb95c0e806f3c40fb6054da32fc3",
+    "url": "/nycity/static/media/imgFive.062efb95.jpg"
+  },
+  {
+    "revision": "36dc7844f7f110ff4a5046645ac61b73",
+    "url": "/nycity/static/media/PlazaITC-Normal.36dc7844.ttf"
+  },
+  {
+    "revision": "0a969adf9fdfd775d04ac9d9c049cd94",
+    "url": "/nycity/static/media/GABRWFFR.0a969adf.TTF"
+  },
+  {
+    "revision": "6a37f4ccbbd4a159f43ba0b8e9cfe80b",
+    "url": "/nycity/static/media/pink.6a37f4cc.png"
+  },
+  {
+    "revision": "50ce103911f2b560e513",
+    "url": "/nycity/static/css/1.9a186255.chunk.css"
+  },
+  {
+    "revision": "5e91e99d115cf4d5bc124a8db52f53ef",
+    "url": "/nycity/index.html"
+  }
+];
